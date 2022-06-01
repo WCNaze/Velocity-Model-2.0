fcef2e47c0885f79237ad1ed4dae02f84de01d92
WCNaze <westchesterpa@thenaze.com>
2022-06-01 19:41:44 -0400

    Initial commit

---
+++ canvas.js
@@ -0,0 +1,158 @@
+var loaded = 0
+console.log(loaded)
+
+//Button toggle
+	var clicked = 1
+	var btn = document.querySelector(".btn");
+
+	function inner(x){
+		var string = x.toString()
+		btn.innerHTML = string
+	}
+
+	function AnimateBtn(){
+		var loaded = 1
+		console.log(loaded)
+		if (clicked == 1) {
+			btn.classList.add("circle");
+			btn.innerHTML = "X";
+			clicked= clicked*-1;
+			ctx2.clearRect(0,0,canvas2.width,canvas2.height);
+			createball();
+		} else if (clicked == -1) {
+			btn.classList.remove("circle");
+			const myTimeout = setTimeout(inner("Start"),1000)
+			/*btn.innerHTML = "Start";*/
+			clicked= clicked*-1;
+			cancelAnimationFrame(ballid)
+			ctx2.clearRect(0,0,canvas2.width,canvas2.height);
+			xvel = 0
+			yvel = 0
+			baseball()
+		}
+	}
+
+
+
+
+
+
+//canvas background
+var canvas = document.getElementById("graph");
+var ch = window.innerHeight*.80
+var cw = window.innerWidth - 20
+var img = document.getElementById("Logo")
+console.log(canvas);
+canvas.width= cw;
+canvas.height= ch;
+
+var ctx = canvas.getContext('2d');
+
+
+//background
+ctx.fillStyle = "rgba(110,110,110,0.5)";
+ctx.fillRect(0, 0, cw, ch);
+
+//x bars
+
+for (let i =0; i<10; i++) {
+ctx.beginPath();
+ctx.moveTo(0, (ch/10)*i);
+ctx.lineTo(cw, (ch/10)*i);
+ctx.strokeStyle = "lightgrey";
+ctx.stroke();
+};
+
+//y bars
+
+for (let i =0; i<10; i++) {
+ctx.beginPath();
+ctx.moveTo((cw/10)*i,0);
+ctx.lineTo((cw/10)*i,ch);
+ctx.strokeStyle = "lightgrey";
+ctx.stroke();
+};
+
+//x - axis
+ctx.beginPath();
+ctx.moveTo(0, ch * 0.5);
+ctx.lineTo(cw, ch * 0.5);
+ctx.lineWidth = "2";
+ctx.strokeStyle = "rgba(110,110,110,1)";
+ctx.stroke();
+
+//y - axis
+ctx.beginPath();
+ctx.moveTo(cw * 0.5, 0);
+ctx.lineTo(cw * 0.5, ch);
+ctx.strokeStyle = "rgba(110,110,110,1)";
+ctx.stroke();
+
+//Layer 1
+
+var canvas2 = document.getElementById("ball");
+/*var ch = window.innerHeight*.80
+var cw = window.innerWidth - 20
+var img = document.getElementById("Logo")
+*/
+console.log(canvas2);
+canvas2.width= cw;
+canvas2.height= ch;
+
+var ctx2 = canvas2.getContext('2d');
+
+//ball
+function baseball(){
+	ctx2.beginPath();
+	ctx2.arc(cw * 0.5, ch * 0.5, 15, 0, Math.PI * 2, false);
+	ctx2.fillStyle = "red";
+	ctx2.fill();
+	ctx2.lineWidth = "2";
+	ctx2.strokeStyle = "darkred";
+	ctx2.stroke();
+}
+
+if (loaded == 0) {
+baseball()
+}
+
+//ball velocity
+var xveli = 10;
+var xvel = xveli;
+var yveli = -5;
+var yvel = yveli;
+
+var ballid;
+
+//ball function
+function createball(){
+	ballid = requestAnimationFrame(createball);
+	ctx2.clearRect(0,0,canvas2.width,canvas2.height);
+	ctx2.beginPath();
+	ctx2.arc(cw * 0.5 + xvel, ch * 0.5 + yvel, 15, 0, Math.PI * 2, false);
+	ctx2.fillStyle = "red";
+	ctx2.fill();
+	ctx2.lineWidth = "2";
+	ctx2.strokeStyle = "darkred";
+	ctx2.stroke();
+	xvel += xveli;
+	yvel += yveli;
+	console.log(loaded)
+}
+
+//Layer 2
+
+var canvas3 = document.getElementById("A+");
+/*
+var ch = window.innerHeight*.80
+var cw = window.innerWidth - 20
+var img = document.getElementById("Logo")
+*/
+console.log(canvas3);
+canvas3.width= cw;
+canvas3.height= ch;
+
+var ctx3 = canvas3.getContext('2d');
+
+//logo
+ctx3.drawImage(img,10,10,cw*.05,cw*.05);
